5120eb471b42a71e8937467a5d1b0bb7aab51c43
Jelle Fresen <jelle@innovattic.com>
2014-01-28 15:53:55 +0100

    Initial commit

---
+++ src/com/innovattic/example/fonts/MainActivity.java
@@ -0,0 +1,16 @@
+package com.innovattic.example.fonts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity);
+	}
+
+}
